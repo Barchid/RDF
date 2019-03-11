04a8a8b832290d21fd9d7cfedb5791e55c094d80
Barchid <sami.barchid@outlook.com>
2019-03-11 02:09:12 +0100

    cc

--- TP07/ACP.R
+++ TP07/ACP.R
@@ -23,71 +23,13 @@ points(x_app[classe_app==3,], col="blue", lwd = lwd_default) # Classe 3 en vert
 
 
 ###################################################
-# Calculer les dispersions inter et intra classe sur 
-# données d'apprentissage
+# ACP sur données d'apprentissage
 ###################################################
-# moyennes classe
-mean1 = colMeans(x_app[classe_app==1,])
-mean2 = colMeans(x_app[classe_app==2,])
-mean3 = colMeans(x_app[classe_app==3,])
-mean = colMeans(x_app)
+# Calcul de la covariance des données d'apprentissage
+covariance_app = cov(x_app)
 
-# covariances intra-classe
-S1 = cov(x_app[classe_app==1,])
-S2 = cov(x_app[classe_app==2,])
-S3 = cov(x_app[classe_app==3,])
-
-# Calcul de la dispersion intra-classe
-Sw=S1+S2+S3
-# Calcul de la dispersion inter-classe
-Sb=(mean1-mean)%*%t(mean1-mean)+(mean2-mean)%*%t(mean2-mean)+(mean3-mean)%*%t(mean3-mean)
-###################################################
-
-
-
-###################################################
-# Effectuer AFD sur les données d'apprentissage
-###################################################
-# Résolution de l'équation pour AFD
-invSw= solve(Sw)
-invSw_by_Sb= invSw %*% Sb
-Vp<- eigen(invSw_by_Sb)
-# Affichage de la droite correspondant au vecteur propre
-# dont la valeur propre la plus ´elev´ee
-pente <- Vp$vectors[2,1]/Vp$vectors[1,1]
-abline(a = 0, b = pente, col = "black")
-###################################################
-
-
-
-###################################################
-# Projection des données d'apprentissage sur l'axe
-# principale trouvé avec AFD
-###################################################
-# Calcul du vecteur d'apprentissage projeté sur l'axe principale
-ScalarProduct_app_AFD = x_app %*% (Vp$vectors[,1])/sqrt(sum(Vp$vectors[,1]*Vp$vectors[,1]))
-
-XP_app = x_app
-# Projection des points
-XP_app[,1]= ScalarProduct_app_AFD * Vp$vectors[1,1]
-XP_app[,2]= ScalarProduct_app_AFD * Vp$vectors[2,1]
-# Affichage des points projetés
-points(XP_app[classe_app==1,], col="red", lwd = lwd_default)
-points(XP_app[classe_app==2,], col="green", lwd = lwd_default)
-points(XP_app[classe_app==3,], col="blue", lwd = lwd_default)
-###################################################
-
-
-
-###################################################
-# Projection des données de test sur l'axe principale
-# obtenu par AFD sur les données d'apprentissage
-###################################################
-
-# Afficher les données de test
-plot(x_test[classe_test==1,], col="red", xlab = "Attribut 1", ylab = "Attribut 2" , xlim=c(-12,12), ylim = c(-8, 10), lwd=lwd_default) # Classe 1 en rouge
-points(x_test[classe_test==2,], col="green", lwd = lwd_default) # Classe 2 en bleu
-points(x_test[classe_test==3,], col="blue", lwd = lwd_default) # Classe 3 en vert
+# Calcul des valeurs et vecteurs propres de covariance
+Vp = eigen(covariance_app)
 
 # Vp$vectors[,1] est le vecteur propre principale
 # Tracer la droite correspondant au vecteur propre principale
@@ -98,28 +40,67 @@ abline(a = 0, b = pente, col = "black")
 
 # Projection des points sur l'axe principale
 # Calcul du vecteur d'apprentissage projeté sur l'axe principale
-ScalarProduct_test_AFD = x_test %*% (Vp$vectors[,1])/sqrt(sum(Vp$vectors[,1]*Vp$vectors[,1]))
+ScalarProduct_app_ACP = x_app %*% (Vp$vectors[,1])/sqrt(sum(Vp$vectors[,1]*Vp$vectors[,1]))
+
+XP_app = x_app
+# Projection des points
+XP_app[,1]= ScalarProduct_app_ACP * Vp$vectors[1,1]
+XP_app[,2]= ScalarProduct_app_ACP * Vp$vectors[2,1]
+# Affichage des points projet´es
+points(XP_app[classe_app==1,], col="red", lwd = lwd_default)
+points(XP_app[classe_app==2,], col="green", lwd = lwd_default)
+points(XP_app[classe_app==3,], col="blue", lwd = lwd_default)
+
+###################################################
+
+
+
+###################################################
+# Afficher les données de test
+###################################################
+plot(x_test[classe_test==1,], col="red", xlab = "Attribut 1", ylab = "Attribut 2" , xlim=c(-12,12), ylim = c(-8, 10), lwd=lwd_default) # Classe 1 en rouge
+points(x_test[classe_test==2,], col="green", lwd = lwd_default) # Classe 2 en bleu
+points(x_test[classe_test==3,], col="blue", lwd = lwd_default) # Classe 3 en vert
+###################################################
+
+
+
+###################################################
+# ACP sur données de test
+###################################################
+
+# Vp$vectors[,1] est le vecteur propre principale
+# Tracer la droite correspondant au vecteur propre principale
+# dont la valeur propre la plus ´elev´ee
+pente <- Vp$vectors[2,1]/Vp$vectors[1,1]
+abline(a = 0, b = pente, col = "black")
+
+
+# Projection des points sur l'axe principale
+# Calcul du vecteur d'apprentissage projeté sur l'axe principale
+ScalarProduct_test_ACP = x_test %*% (Vp$vectors[,1])/sqrt(sum(Vp$vectors[,1]*Vp$vectors[,1]))
 
 XP_test = x_test
 # Projection des points
-XP_test[,1]= ScalarProduct_test_AFD * Vp$vectors[1,1]
-XP_test[,2]= ScalarProduct_test_AFD * Vp$vectors[2,1]
+XP_test[,1]= ScalarProduct_test_ACP * Vp$vectors[1,1]
+XP_test[,2]= ScalarProduct_test_ACP * Vp$vectors[2,1]
 # Affichage des points projet´es
 points(XP_test[classe_test==1,], col="red", lwd = lwd_default)
 points(XP_test[classe_test==2,], col="green", lwd = lwd_default)
 points(XP_test[classe_test==3,], col="blue", lwd = lwd_default)
-###################################################
-
-
 
 ###################################################
-# ALD sur les données de test projetées sur l'axe principale
+
+
+
+###################################################
+# Appliquer, sur les données de test, la LDA (règle de décision) obtenues grâce aux données d'apprentissage 
 ###################################################
 # Appliquer la LDA sur les données d'apprentissage
-x_app_AFD.lda = lda(ScalarProduct_app_AFD,classe_app)
+x_app_ACP.lda = lda(ScalarProduct_app_ACP,classe_app)
 
 # Appliquer à partir des données d'apprentissage
-assigne_test = predict(x_app_AFD.lda, newdata=ScalarProduct_test_AFD)
+assigne_test = predict(x_app_ACP.lda, newdata=ScalarProduct_test_ACP)
 
 # Estimation des taux de bonnes classifications
 table_classification_test = table(classe_test, assigne_test$class)
@@ -150,4 +131,3 @@ shape[assigne_test$class==3]=3 ; # Classe 3 = croix
 plot(x_test,col=couleur,pch=shape, xlab = "Attribut 1", ylab = "Attribut 2" , xlim=c(-12,12), ylim = c(-8, 10), lwd = lwd_default)
 pente <- Vp$vectors[2,1]/Vp$vectors[1,1]
 abline(a = 0, b = pente, col = "black")
-###################################################
---
+++ TP07/Q9_ALD_espace_origine.R
@@ -0,0 +1,156 @@
+library("MASS")
+library("lattice")
+load(file = "x_app.data")
+load(file = "classe_app.data")
+load(file = "x_test.data")
+load(file = "classe_test.data")
+
+#Valeur pour mettre plus en valeur les screens pour le rapport
+lwd_default <- 3
+
+##########################################
+# Q1.1
+##########################################
+#Affichage de l'ensemble d'apprentissage
+couleur_app<-rep('red',length(x_app))
+couleur_app[classe_app==1]='red'
+couleur_app[classe_app==2]='green'
+couleur_app[classe_app==3]='blue'
+
+plot(x_app, col = couleur_app, lwd = lwd_default)
+
+#Affichage de l'ensemble de test
+couleur_test<-rep('red',length(x_test))
+couleur_test[classe_test==1]='red'
+couleur_test[classe_test==2]='green'
+couleur_test[classe_test==3]='blue'
+plot(x_test, col = couleur_test, lwd = lwd_default)
+
+##########################################
+# Q1.2
+##########################################
+#Affichage de l'ensemble d'apprentissage
+plot(x_app, col = couleur_app, lwd = lwd_default)
+
+# Déclaration des vecteurs moyennes
+M1 = seq(1,2)
+M2 = seq(1,2)
+M3 = seq(1,2)
+
+# Vecteur moyenne de la classe 1
+M1[1] = mean(x_app[classe_app==1,1])
+M1[2] = mean(x_app[classe_app==1,2])
+
+# Vecteur moyenne de la classe 2
+M2[1] = mean(x_app[classe_app==2,1])
+M2[2] = mean(x_app[classe_app==2,2])
+
+# Vecteur moyenne de la classe 3
+M3[1] = mean(x_app[classe_app==3,1])
+M3[2] = mean(x_app[classe_app==3,2])
+
+points(M1[1], y = M1[2], pch = 20, col = "red", lwd=15)
+points(M2[1], y = M2[2], pch = 20, col = "green", lwd=15)
+points(M3[1], y = M3[2], pch = 20, col = "blue", lwd=15)
+
+#Affichage de l'ensemble d'apprentissage
+plot(x_app, col = couleur_app, lwd = lwd_default)
+
+# Matrices de co variance
+Sigma1 <- matrix(1,2,2)
+Sigma2 <- matrix(1,2,2)
+Sigma3 <- matrix(1,2,2)
+
+for (i in 1:2) {
+  for (j in 1:2) {
+    Sigma1[i,j]=cov(as.vector(x_app[classe_app==1,i]), as.vector(x_app[classe_app==1,j]))
+    Sigma2[i,j]=cov(as.vector(x_app[classe_app==2,i]), as.vector(x_app[classe_app==2,j]))
+    Sigma3[i,j]=cov(as.vector(x_app[classe_app==3,i]), as.vector(x_app[classe_app==3,j]))
+  }
+}
+
+##########################################
+# Q1.3
+##########################################
+
+# Grille d'estimation de la densit´e de probabilit´e en 50 intervalles selon 1er attribut
+xp1 <- seq(min(x_app[,1]),max(x_app[,1]),length=50)
+# Grille d'estimation de la densit´e de probabilit´e en 50 intervalles selon 2eme attribut
+xp2 <- seq(min(x_app[,2]),max(x_app[,2]),length=50)
+
+grille <- expand.grid(x1=xp1,x2=xp2)
+x_app_lda<-lda(x_app,classe_app)
+
+# Estimation des densit´es de probabilit´es a posteriori dans Zp
+grille = cbind(grille[,1],grille[,2])
+Zp <- predict(x_app_lda,grille)
+
+#Affichage des lignes de décision
+zp_3<-Zp[["posterior"]][,3]-pmax(Zp[["posterior"]][,1],Zp[["posterior"]][,2])
+zp_2<-Zp[["posterior"]][,2]-pmax(Zp[["posterior"]][,1],Zp[["posterior"]][,3])
+
+contour(xp1,xp2,matrix(zp_3,50),add=TRUE,levels=0,drawlabels=FALSE, col="green", lwd = lwd_default)
+contour(xp1,xp2,matrix(zp_2,50),add=TRUE,levels=0,drawlabels=FALSE, col="blue", lwd = lwd_default)
+
+##########################################
+# Q1.4
+##########################################
+
+assigne_test <- predict(x_app_lda, newdata=x_test)
+# Estimation des taux de bonnes classifications
+table_classification_test_lda <-table(classe_test, assigne_test[["class"]])
+# table of correct class vs. classification
+diag(prop.table(table_classification_test_lda, 1))
+# total percent correct
+taux_bonne_classif_test_lda <-sum(diag(prop.table(table_classification_test_lda)))
+
+# Création du vecteur contenant le code de la forme des données test assignées aux
+shape<-rep(1,length(x_test)) ;
+# Forme des données assignées 
+shape[assigne_test[["class"]]==1]=1 ;
+shape[assigne_test[["class"]]==2]=2 ;
+shape[assigne_test[["class"]]==3]=3 ;
+plot(x_test,col=couleur_test,pch=shape,xlab = "X1", ylab = "X2", lwd = lwd_default)
+
+#Affichage des lignes de décision
+contour(xp1,xp2,matrix(zp_3,50),add=TRUE,levels=0,drawlabels=FALSE, col="black", lwd = lwd_default)
+contour(xp1,xp2,matrix(zp_2,50),add=TRUE,levels=0,drawlabels=FALSE, col="black", lwd = lwd_default)
+
+# ##########################################
+# # Q1.5
+# ##########################################
+# #Affichage de l'ensemble d'apprentissage
+# plot(x_app, col = couleur_app, lwd = lwd_default)
+# 
+# x_app_qda<-qda(x_app,classe_app)
+# 
+# # Estimation des densit´es de probabilit´es a posteriori dans Zp
+# Zp <- predict(x_app_qda,grille)
+# 
+# #Affichage des lignes de décision
+# zp_3<-Zp[["posterior"]][,3]-pmax(Zp[["posterior"]][,1],Zp[["posterior"]][,2])
+# zp_2<-Zp[["posterior"]][,2]-pmax(Zp[["posterior"]][,1],Zp[["posterior"]][,3])
+# contour(xp1,xp2,matrix(zp_3,50),add=TRUE,levels=0,drawlabels=FALSE, col="green", lwd = lwd_default)
+# contour(xp1,xp2,matrix(zp_2,50),add=TRUE,levels=0,drawlabels=FALSE, col="blue", lwd = lwd_default)
+# 
+# assigne_test <- predict(x_app_qda, newdata=x_test)
+# # Estimation des taux de bonnes classifications
+# table_classification_test_qda <-table(classe_test, assigne_test[["class"]])
+# # table of correct class vs. classification
+# diag(prop.table(table_classification_test_qda, 1))
+# # total percent correct
+# taux_bonne_classif_test_qda <-sum(diag(prop.table(table_classification_test_qda)))
+# 
+# # Création du vecteur contenant le code de la forme des données test assignées aux
+# shape<-rep(1,length(x_test)) ;
+# # Forme des données assignées 
+# shape[assigne_test[["class"]]==1]=1 ;
+# shape[assigne_test[["class"]]==2]=2 ;
+# shape[assigne_test[["class"]]==3]=3 ;
+# # Affichage avec code couleur et forme adapt´ees
+# plot(x_test,col=couleur_test,pch=shape,xlab = "X1", ylab = "X2", lwd = lwd_default)
+# 
+# #Affichage des lignes de décision
+# contour(xp1,xp2,matrix(zp_3,50),add=TRUE,levels=0,drawlabels=FALSE, col="green", lwd = lwd_default)
+# contour(xp1,xp2,matrix(zp_2,50),add=TRUE,levels=0,drawlabels=FALSE, col="blue", lwd = lwd_default)
+
